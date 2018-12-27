9ccfa2961bead8095be328cb119371d8cb824669
un-install <elllipsse@gmail.com>
2018-12-27 22:12:16 +0200

    asd
    asd
    
    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("somethiasdnzsg1aa");
+        System.out.println("sq 1");
+        System.out.println("sq 2");
     }
 }
